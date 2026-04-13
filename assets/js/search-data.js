e685c0c008ccd8815a9e89f4d64d53fdcd829e40
txin <titian.tang@gmail.com>
2026-04-13 23:24:29 +0000

    Deploying to gh-pages from @ txin/txin.github.io@f10b0c4858cde325235336a445fa807203fdbeb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,4 +56,7 @@ ninja.data = [{
           section: "News",},{id: "news-talk-proposal-sprinkle-differential-privacy-on-a-bit-of-everything-was-accepted-to-real-world-crypto-2026-looking-forward-to-visiting-taipei-in-march-2026-unfortunately-the-other-submission-on-learning-functionality-outputs-was-not-accepted",
           title: 'Talk proposal “Sprinkle Differential Privacy on a Bit of Everything” was accepted to...',
           description: "",
+          section: "News",},{id: "news-started-a-new-position-at-the-university-of-glasgow-uk-back-in-the-uk-did-my-undergrad-in-bristol-but-this-time-in-scotland-feel-free-to-email-me-if-you-would-like-to-have-a-research-random-chat-and-are-around-definitely-recommend-the-fringe-festival-in-edinburgh-in-august-i-ve-been-there-twice-before-and-planning-to-travel-there-from-glasgow-again-this-time",
+          title: 'Started a new position at the University of Glasgow, UK. Back in the...',
+          description: "",
           section: "News",},];
